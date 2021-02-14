d6935b66dd5dc2cd0978d1dc472dd88e51974ecc
coding-to-music <connors.tom@gmail.com>
2021-02-14 05:53:37 -0500

    installed package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,8 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinxcontrib.twitter'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
