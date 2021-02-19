f447667b62e3be91e3557184599350cd1db9a2ba
coding-to-music <connors.tom@gmail.com>
2021-02-19 16:14:56 -0500

    build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,10 @@
 #
 import os
 import sys
-# import sphinx_gallery
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+import sphinx_gallery
 
 
 # -- Project information -----------------------------------------------------
