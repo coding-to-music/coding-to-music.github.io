fea0036b1dd719e62b4b873efb2a079a072d0a30
coding-to-music <connors.tom@gmail.com>
2021-04-22 17:53:05 +0000

    build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -292,6 +292,7 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+html_extra_path = ["_html"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
