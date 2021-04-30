2667676b8dc1839b50867296a85891760c66646d
coding-to-music <connors.tom@gmail.com>
2021-04-30 23:52:23 +0000

    build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -141,7 +141,6 @@ extensions = [
     "sphinxcontrib.nwdiag",
     "sphinxcontrib.plantuml",
     "sphinxcontrib.seqdiag",
-    "sphinxcontrib.twitter",
     "sphinx_issues",
     'sphinx-prompt',
     "sphinxext.opengraph",
