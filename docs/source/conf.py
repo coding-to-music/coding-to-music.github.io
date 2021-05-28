6d52e7ceab0bd3bbb6c0bed60ebec30e0ff028e1
coding-to-music <connors.tom@gmail.com>
2021-05-28 21:29:10 +0000

    templates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -364,7 +364,7 @@ rst_epilog = """
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['ntemplates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
