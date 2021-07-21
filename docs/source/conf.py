866d25bb9cea2d6c45551f8ff142a07ddfd965e2
coding-to-music <connors.tom@gmail.com>
2021-07-21 20:12:32 +0000

    html-theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -223,20 +223,43 @@ html_short_title = project
 # material theme options (see theme.conf for more information)
 
 base_url = 'https://pandemic-overview.readthedocs.io'
-base_url += 'latest/' if full_version == version else 'devel/'
+# base_url += 'latest/' if full_version == version else 'devel/'
 html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+
+    'nav_title': 'pandemic-overview',
+    # 'nav_title': 'pandemic-overview {0}'.format(version),
+    # 'nav_title': 'statsmodels {0}'.format(version),
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
     'base_url': base_url,
+
+
+    # Set the repo location to get a badge with stats
     'repo_url': 'https://github.com/coding-to-music/coding-to-music.github.io',
     'repo_name': 'coding-to-music.github.io',
+
+
+    # Visible levels of the global TOC; -1 means unlimited
     'globaltoc_depth': 3,
-    'globaltoc_collapse': True,
+    # If False, expand all TOC entries
+    # 'globaltoc_collapse': True,
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
     'globaltoc_includehidden': True,
+
+
+    # Set the color and the accent color
     'color_primary': 'indigo',
     'color_accent': 'blue',
-    'nav_title': 'pandemic-overview {0}'.format(version),
-    # 'nav_title': 'statsmodels {0}'.format(version),
+
     'master_doc': False,
+
+    # I don't know about these items
     'nav_links': [],
+
     'heroes': {'index': 'statistical models, hypothesis tests, and data '
                         'exploration',
                'examples/index': 'examples and tutorials to get started with '
@@ -248,7 +271,36 @@ html_theme_options = {
 language = 'en'
 html_last_updated_fmt = ''
 
+html_theme = 'sphinx_material'
 
+# Material theme options (see theme.conf for more information)
+html_material_theme_options_example = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Project Name',
+
+    # Set you GA account ID to enable tracking
+    'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://project.github.io/project',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/project/project/',
+    'repo_name': 'Project',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
 
 #    "sphinxcontrib.twitter",
 #   "sphinx_rediraffe",
