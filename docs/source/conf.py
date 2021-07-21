0383fe7ade92ca6a679ed65d7b24ad33d6d23e6e
coding-to-music <connors.tom@gmail.com>
2021-07-21 20:41:40 +0000

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -274,36 +274,36 @@ html_last_updated_fmt = ''
 html_theme = 'sphinx_material'
 
 # Material theme options (see theme.conf for more information)
-html_material_theme_options_example = {
+# html_material_theme_options_example = {
 
     # this is needed, is available from RTD them
-    'prev_next_buttons_location': 'both'
+    # 'prev_next_buttons_location': 'both'
 
     # Set the name of the project to appear in the navigation.
-    'nav_title': 'Project Name',
+    # 'nav_title': 'Project Name',
 
     # Set you GA account ID to enable tracking
-    'google_analytics_account': 'UA-XXXXX',
+    # 'google_analytics_account': 'UA-XXXXX',
 
     # Specify a base_url used to generate sitemap.xml. If not
     # specified, then no sitemap will be built.
-    'base_url': 'https://project.github.io/project',
+    # 'base_url': 'https://project.github.io/project',
 
     # Set the color and the accent color
-    'color_primary': 'blue',
-    'color_accent': 'light-blue',
+    # 'color_primary': 'blue',
+    # 'color_accent': 'light-blue',
 
     # Set the repo location to get a badge with stats
-    'repo_url': 'https://github.com/project/project/',
-    'repo_name': 'Project',
+    # 'repo_url': 'https://github.com/project/project/',
+    # 'repo_name': 'Project',
 
     # Visible levels of the global TOC; -1 means unlimited
-    'globaltoc_depth': 3,
+    # 'globaltoc_depth': 3,
     # If False, expand all TOC entries
-    'globaltoc_collapse': False,
+    # 'globaltoc_collapse': False,
     # If True, show hidden TOC entries
-    'globaltoc_includehidden': False,
-}
+    # 'globaltoc_includehidden': False,
+# }
 
 #    "sphinxcontrib.twitter",
 #   "sphinx_rediraffe",
