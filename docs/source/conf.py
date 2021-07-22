c8a28ba69f63c1e5078ca299bc63522b8ab5f901
coding-to-music <connors.tom@gmail.com>
2021-07-22 04:37:03 +0000

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -270,6 +270,67 @@ html_theme_options = {
 
 #     'prev_next_buttons_location': 'both'
 
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = '_static/images/statsmodels/statsmodels-logo-v2-bw.svg'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = '_static/images/statsmodels/favicon.ico'
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+# html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+# html_use_smartypants = True
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
+# Additional templates that should be rendered to pages, maps page names to
+# template names.
+# html_additional_pages = {}
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+# html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+# html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+# html_show_sourcelink = True
+
+# If true, 'Created using Sphinx' is shown in the HTML footer. Default is True.
+# html_show_sphinx = True
+
+# If true, '(C) Copyright ...' is shown in the HTML footer. Default is True.
+# html_show_copyright = True
+
+# If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+# html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. '.xhtml').
+# html_file_suffix = None
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'pandemic-overview-doc'
+
 language = 'en'
 html_last_updated_fmt = ''
 
