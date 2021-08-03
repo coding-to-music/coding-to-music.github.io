236e154f28b38be617a870680d23047cb7312c26
coding-to-music <connors.tom@gmail.com>
2021-08-03 22:24:34 +0000

    logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -299,7 +299,8 @@ material_html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/images/statsmodels/statsmodels-logo-v2-bw.svg'
+# html_logo = '_static/images/statsmodels/statsmodels-logo-v2-bw.svg'
+html_logo = '_static/ska-telescope/logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
